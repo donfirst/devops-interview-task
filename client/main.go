1e02255fd48dbae5e0bb8ce5aee2db43ebff0616
donfirst <20359212+donfirst@users.noreply.github.com>
2024-03-05 10:49:49 +0000

    Update main.go

--- client/main.go
+++ client/main.go
@@ -4,10 +4,26 @@ import (
 	"context"
 	"log/slog"
 	"os"
+	"time"
 
 	"github.com/muzzapp/devops-interview-task/pkg/muzz"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	grpcCalls = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "grpc_calls_total",
+		Help: "Total number of gRPC calls",
+	}, []string{"method", "status"})
+
+	grpcLatency = promauto.NewHistogram(prometheus.HistogramOpts{
+		Name:    "grpc_call_latency_seconds",
+		Help:    "Latency of gRPC calls",
+		Buckets: prometheus.DefBuckets,
+	})
 )
 
 func main() {
@@ -19,14 +35,20 @@ func main() {
 
 	client := muzz.NewServiceClient(conn)
 
+	start := time.Now()
 	response, respErr := client.Echo(
 		context.Background(),
 		&muzz.EchoRequest{Message: "Hello, world!"},
 	)
+	elapsed := time.Since(start)
+	grpcLatency.Observe(elapsed.Seconds())
+
 	if respErr != nil {
 		slog.Error("Failed to call gRPC service: %v", "err", respErr)
+		grpcCalls.With(prometheus.Labels{"method": "Echo", "status": "fail"}).Inc()
 		os.Exit(1)
 	}
 
 	slog.Info("Response: %s", "message", response.Message)
+	grpcCalls.With(prometheus.Labels{"method": "Echo", "status": "success"}).Inc()
 }
